ec01c4c7ec09ae523b5a5c53dff680818bb8b3e9
mariobot <mariobot113@hotmail.com>
2026-05-25 16:59:25 -0500

    FIrst protection some init code.

--- PolyMyMarket.Web/Program.cs
+++ PolyMyMarket.Web/Program.cs
@@ -1,10 +1,13 @@
 using PolyMyMarket.Web.Components;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddRadzenComponents();
 
 var app = builder.Build();
 
